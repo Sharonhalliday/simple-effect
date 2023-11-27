b724650dabe5a676efea5a4f7046eb2eba9da191
sharonhalliday <hallidaysharon5@gmail.com>
2023-11-27 13:48:24 -0800

    second commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $("p").click(function () {
+        $(".walrus-showing").toggle();
+        $(".walrus-hidden").toggle();
+    });
+    $("p").click(function () {
+        $(".walrus-showing").fadeIn();
+        $(".walrus-hidden").fadeIn();
+    });
+    //  $("p").click(function () {
+    //     $(".walrus-showing").fadeOut();
+    //     $(".walrus-hidden").fadeOut();
+    // });
+    // $("p").click(function () {
+    //     $(".walrus-showing").fadeToggle();
+    //     $(".walrus-hidden").fadeToggle();
+    // });
+    // $("p").click(function () {
+    //     $(".walrus-showing").slideDown();
+    //     $(".walrus-hidden").slideDown();
+    // });
+    // $("p").click(function () {
+    //     $(".walrus-showing").slideUp();
+    //     $(".walrus-hidden").slideUp();
+    // });
+    // $("p").click(function() {
+    //     $(".walrus-showing").slideToggle();
+    //     $(".walrus-hidden").slideToggle();
+    //   });
+});
